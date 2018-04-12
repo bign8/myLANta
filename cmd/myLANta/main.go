51a187c64b119fcb1cd589655836344054a04a44
Nate <big.nate.w@gmail.com>
2018-04-12 09:19:30 -0500

    Adding web handler

--- cmd/myLANta/main.go
+++ cmd/myLANta/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/bign8/myLANta/mylanta"
+	"github.com/bign8/myLANta/web"
 )
 
 var portz = flag.String("PORT", "8080", "port to serve on")
@@ -44,7 +45,7 @@ func main() {
 	flag.Parse()
 	log.Println("Serving on :" + *portz)
 	go func() {
-		panic(http.ListenAndServe(":"+*portz, nil))
+		panic(http.ListenAndServe(":"+*portz, web.New(network)))
 	}()
 
 	buf := make([]byte, 1)
