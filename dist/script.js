7c82a796d75b00786308746a0dd8c3de415741be
Nate <big.nate.w@gmail.com>
2018-04-29 11:30:56 -0600

    Revamping javascript to not pollute global namespace

--- dist/script.js
+++ dist/script.js
@@ -1,23 +1,51 @@
-function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
+(function(d) {
+  function ready(fn) {
+    if (d.attachEvent ? d.readyState === "complete" : d.readyState !== "loading"){
+      fn();
+    } else {
+      d.addEventListener('DOMContentLoaded', fn);
+    }
   }
-}
 
-ready(function() {
-  var form = document.forms[0],
-    save = form.querySelector("[type=submit]"),
-    file = form.querySelector("[type=file]"),
-    best = document.createElement('input');
+  // Initialize the file upload form.
+  function initFile(name) {
+    var form = d.forms[name],
+      save = form.querySelector("[type=submit]"),
+      file = form.querySelector("[type=file]"),
+      best = d.createElement('input');
 
-  best.type = 'button';
-  best.value = save.value;
-  file.style.display = 'none';
-  save.style.display = 'none';
+    best.type = 'button';
+    best.value = save.value;
+    file.style.display = 'none';
+    save.style.display = 'none';
 
-  form.appendChild(best);
-  best.onclick = function(e) { file.click(e); };
-  file.onchange = function(e) { form.submit(e); };
-});
+    form.appendChild(best);
+    best.onclick = function(e) { file.click(e); };
+    file.onchange = function(e) { form.submit(e); };
+  }
+
+  // Initialize the chat form.
+  function initChat(name) {
+    var form = d.forms[name],
+      send = form.querySelector("[type=submit]"),
+      name = form.querySelector("[name=who]"),
+      text = form.querySelector("[name=msg]");
+
+    name.value = window.localStorage["name"];
+
+    send.onclick = function(e) {
+      window.localStorage["name"] = name.value; // save username
+
+      // wait till after form submits to clear text field
+      window.setTimeout(function() {
+        text.value = '';
+      }, 1);
+    };
+  }
+
+  // When the document is ready, run some initialization scrips.
+  ready(function() {
+    initFile('file');
+    initChat('chat');
+  });
+})(document);
